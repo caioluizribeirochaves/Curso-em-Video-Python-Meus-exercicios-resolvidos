8deacadcf5fe81f04315ae914bebda042f4e2e14
Caio Luiz <caioriberio@gmail.com>
2026-04-28 11:06:08 -0300

    Python POO

---
+++ PythonPOO/ex001/ex001.py
@@ -0,0 +1,19 @@
+# Declaração de classe:
+class Gafanhoto:
+    def __init__(self): # Método construtor
+        # Atributos de Instância
+        self.nome = ''
+        self.idade = 0
+
+    # Métodos de Instância
+    def aniversario(self):
+        self.idade += 1
+
+    def mensagem(self):
+        return f'{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade.'
+
+# Declaração de objetos:
+g1 = Gafanhoto()
+g1.nome = 'Caio'
+g1.idade = 27
+print(g1.mensagem())
