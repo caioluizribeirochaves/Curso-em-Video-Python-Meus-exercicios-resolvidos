a3c1679a5c98d0a253038b7626baeeb012ae5b26
Caio Luiz <caioriberio@gmail.com>
2026-04-30 09:50:39 -0300

    Learning about Rich

---
+++ PythonPOO/exercicios/ExtraRich/rich001.py
@@ -0,0 +1,5 @@
+from rich import print
+print('Olá, [red]Mundo[/]! :earth_americas:')
+print('Olá, [bold blue on white]Pequeno Gafanhoto[/] :vulcan_salute:')
+print(':+1: :-1:')
+# python -m rich.emoji - Comando para mostrar a lista de emoji com os respectivos nomes
---
+++ PythonPOO/exercicios/ExtraRich/rich002.py
@@ -0,0 +1,6 @@
+from rich import print
+from rich.panel import Panel
+
+caixa = Panel('[white]Esse aqui é um painel de exemplo[/]:+1:', title='MENSAGEM', style='red', width=35)
+# width - largura, title - titulo do painel, style - cor do painel
+print(caixa)
