adcf5f375b49420a222f25f964e2f23cad704d69
Caio Luiz <caioriberio@gmail.com>
2026-04-30 16:55:25 -0300

    Update desafio017.py

--- PythonPOO/desafios/desafio017.py
+++ PythonPOO/desafios/desafio017.py
@@ -1,1 +1,8 @@
-# Crie a classe Produto, onde podemos cadastrar nome e preço. Crie também um método que msotre uma etiqueta de preço do produto.
+# Crie a classe Produto, onde podemos cadastrar nome e preço. Crie também um método que mostre uma etiqueta de preço do produto.
+class Produto:
+    def __init__(self, nome='<Desconhecido>', preco=0):
+        self.produto = nome
+        self.valor = preco
+
+
+
