b39b16211ca368e408c40af582f7d26d73e7ae2d
Caio Luiz <caioriberio@gmail.com>
2026-05-02 19:42:48 -0300

    updates

--- PythonPOO/desafios/desafio017/desafio017.py
+++ PythonPOO/desafios/desafio017/desafio017.py
@@ -1,8 +1,23 @@
 # Crie a classe Produto, onde podemos cadastrar nome e preço. Crie também um método que mostre uma etiqueta de preço do produto.
+from rich import print
+from rich.panel import Panel
+
 class Produto:
     def __init__(self, nome='<Desconhecido>', preco=0):
         self.produto = nome
         self.valor = preco
 
+    def etiqueta(self):
+        conteudo = f'{self.produto.center(30, ' ')}'
+        conteudo += f'{'-' * 30}'
+        valorf = f'R${self.valor:,.2f}'
+        conteudo += f'{valorf.center(30, '.')}'
+        return Panel(conteudo, title='Produto', width=34)
 
+p1 = Produto('Monitor LG 34G600B-A', 2_000)
+p2 = Produto('TV QN90F', 3_190)
+p3 = Produto()
 
+print(p1.etiqueta())
+print(p2.etiqueta())
+print(p3.etiqueta())
--- PythonPOO/desafios/desafio018/desafio018.py
+++ PythonPOO/desafios/desafio018/desafio018.py
@@ -2,3 +2,73 @@
 # Considere:
 # Consumo padrão: 400g por pessoa
 # Preço: R$82,40/kg
+from rich import print
+from rich.panel import Panel
+
+class Churrasco:
+    def __init__(self,titulo, quantidade=0):
+        self.titulo = titulo
+        self.quantidade = quantidade
+
+    def calculo(self):
+        return self.quantidade * 0.4
+
+    def custo(self):
+        return self.calculo() * 82.40
+
+    def custopessoa(self):
+        return self.custo() / self.quantidade
+
+    def analisar(self):
+        titulo = self.titulo
+        dados = f'Analisando [green]{self.titulo}[/] com [blue]{self.quantidade} convidados[/]'
+        dados += '\nCada Participante comerá 0.4Kg e cada Kg Custa R$82.40'
+        dados += f'\nRecomendo [blue]comprar {self.calculo():.3f}Kg[/] de carne'
+        dados += f'\nO custo total será de [green]R${self.custo():.2f}[/]'
+        dados += f'\nCada pessoa pagará [yellow]R${self.custopessoa()}[/] para participar.'
+        return Panel(dados, title=titulo)
+
+
+c1 = Churrasco('Churras dos Brothers', 10)
+print(c1.analisar())
+
+c2 = Churrasco('Churras dos Amigos', 15)
+print(c2.analisar())
+
+#
+#
+# from rich import print
+# from rich.table import Table
+#
+#
+# class Churrasco:
+#     preco_carne:float = 82.4 #82,4R$/kg
+#     kg_carne_pessoa:float = 0.4 #400g/pessoa
+#
+#
+#     def __init__(self, titulo, pessoas):
+#         self.title=titulo
+#         self.people=pessoas
+#
+#
+#     def carne(self):
+#         total_carne = self.people * Churrasco.kg_carne_pessoa
+#         total_churras = total_carne * self.__class__.preco_carne
+#         preco_pessoa = total_churras/self.people
+#         #return (f" O total de carne será {total_carne} Kgs e o valor do churrasco é R$ {total_churras} \n "
+#                 #f"Para {self.people} pessoas, o valor do churrasco é R$ {preco_pessoa} cada")
+#         tabela = Table(title=f"[bold red]{self.title}".center(30), width=80,style='blue',)
+#         tabela.add_column(f"[yellow]Qtd. Pessoas",justify="center", width= 7, style='green')
+#         tabela.add_column("[yellow]Total de Carne (Kg)",justify='center', width=12, style='green')
+#         tabela.add_column("[yellow]Total do Churrasco (R$)",justify='center', width=14, style='green')
+#         tabela.add_column("[yellow]Valor por Pessoa (R$)", justify='center', width=12, style='green')
+#         tabela.add_row(f"{self.people}",
+#                        f"{total_carne:.2f}".replace('.',','),
+#                        f"{total_churras:.2f}".replace('.',','),
+#                        f"{preco_pessoa:.2f}".replace('.',','))
+#         print(tabela)
+#
+# churras1 = Churrasco("Churras dos Brothers", 15)
+# churras2 = Churrasco("Aniversário Bombástico", 50)
+# churras1.carne()
+# churras2.carne()
