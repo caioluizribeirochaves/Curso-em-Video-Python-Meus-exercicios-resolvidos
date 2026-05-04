b37c6611483c147b44a85f5fcf58a34c6675a891
Caio Luiz <caioriberio@gmail.com>
2026-05-03 21:38:17 -0300

    Update desafio019.py

--- PythonPOO/desafios/desafio019/desafio019.py
+++ PythonPOO/desafios/desafio019/desafio019.py
@@ -1,1 +1,33 @@
 # Crie uma classe livro, que vai simular a passagem de páginas de um livro, considerando também se o usuário chegou no fim da leitura
+from turtledemo.round_dance import stop
+
+from rich import print
+from time import sleep
+
+class Livro:
+    def __init__(self, titulo='<Desconhecido>', paginas=0):
+        self.titulo = titulo
+        self.paginas = paginas
+        self.pagina_atual = 1
+
+        print(f':open_book: [blue]Você acabou de abrir o livro [red]{self.titulo}[/] que tem [green]{self.paginas} paginas[/] no total. Você agora está na [yellow]página {self.pagina_atual}[/][/]')
+
+    def avancar_paginas(self, qtd=0):
+        cont = 0
+        for pg in range(0, qtd, 1):
+            if not self.fim_do_livro():
+                self.pagina_atual += 1
+                print(f'Pág{self.pagina_atual} :arrow_forward:', end=' ')
+                sleep(0.3)
+                cont += 1
+        print(f'[blue]Você avançou [bold]{qtd}[/] páginas e agora está na [yellow]página {self.pagina_atual}[/]')
+        if self.fim_do_livro():
+            print(f':closed_book: [red]Você chegou ao final do livro [bold]{self.titulo}[/][/]')
+
+    def fim_do_livro(self) -> bool:
+        return True if self.pagina_atual == self.paginas else False
+
+
+l1 = Livro('10 coisas que aprendi', 20)
+l1.avancar_paginas(3)
+l1.avancar_paginas(30)
