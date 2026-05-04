ae449f30f1a63b78731de0c896eefb4234cfbfd0
Caio Luiz <caioriberio@gmail.com>
2026-05-04 10:40:57 -0300

    ex020-022

--- PythonPOO/desafios/desafio020/desafio020.py
+++ PythonPOO/desafios/desafio020/desafio020.py
@@ -1,2 +1,30 @@
 # Crie a classe Gamer, onde podemos cadastrar, nome, nick e os jogos favoritos de uma pessoa.
 # Crie também um método que permita mostrar a ficha desse gamer.
+from rich import print
+from rich.panel import Panel
+
+class Gamer:
+    def __init__(self, nome='<Desconhecido>', nick='<Desconhecido>'):
+        self.nome = nome
+        self.nick = nick
+        self.favoritos = list()
+
+    def add_favoritos(self, game):
+        self.favoritos.append(game)
+        self.favoritos = sorted(self.favoritos, key=str.lower)
+
+    def ficha(self):
+        dados = (f'Nome real: [black on blue]{self.nome}[/]')
+        dados += (f'\nJogos favoritos:')
+        for num, game in enumerate(self.favoritos):
+            dados += f'\n:video_game: [blue]{game}[/]'
+        painel = Panel(dados, title=f'Jogador <[bold]{self.nick}[/]>', width=40)
+        print(painel)
+
+
+
+j1 = Gamer('Caio Luiz', 'Caiebas')
+j1.add_favoritos('God Of war')
+j1.add_favoritos('Crash Bandicoot')
+j1.ficha()
+
--- PythonPOO/desafios/desafio021/desafio021.py
+++ PythonPOO/desafios/desafio021/desafio021.py
@@ -1,1 +1,42 @@
 # Crie a classe Caneta, que simule o funcionamento de uma caneta colorida, podendo escrever frases na cor relativa.
+from rich import print
+
+class Caneta:
+    def __init__(self, cor='azul'):
+        escolha = ''
+        match cor.lower().strip():
+            case 'azul':
+                escolha = '[blue]'
+            case 'vermelho' | 'vermelha':
+                escolha = '[red]'
+            case 'verde':
+                escolha = '[green]'
+            case _:
+                escolha = '[white]'
+        self.cor = escolha
+        self.tampada = True
+
+    def escrever(self, msg):
+        if self.tampada:
+            print(f':prohibited: A {self.cor}caneta[/] está tampada!')
+        else:
+            print(f'{self.cor}{msg}[/]', end='')
+
+    def quebrar_linha(self, qtd=1):
+        print('\n' * qtd, end='')
+
+    def tampar(self):
+        self.tampada = True
+
+    def destampar(self):
+        self.tampada = False
+
+c1 = Caneta('azul')
+c2 = Caneta('vermelho')
+c3 = Caneta('verde')
+c1.destampar()
+c2.destampar()
+c3.destampar()
+c1.escrever('Olá Mundo!')
+c2.escrever('Tudo certo?')
+c3.escrever('Estou aprendendo muita coisa massa!')
