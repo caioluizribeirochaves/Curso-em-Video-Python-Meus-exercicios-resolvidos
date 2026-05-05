216fcc85e9a597deb6c123442a9bfca78903fc40
Caio Luiz <caioriberio@gmail.com>
2026-05-05 10:13:57 -0300

    heranças

---
+++ PythonPOO/exercicios/ex005/__main__.py
@@ -0,0 +1,15 @@
+from rich import print, inspect
+from classesex005 import Aluno, Professor, Funcionario
+
+a1 = Aluno('Caio', 27, 'ADS', 'EAD')
+a1.fazer_aniversario()
+a1.fazer_matricula()
+inspect(a1, methods=True)
+
+p1 = Professor('Guanabara', 55, 'Sabe Tudo', 'GOD')
+p1.dar_aula()
+inspect(p1, methods=True)
+
+f1 = Funcionario('Creuza', 67, 'RH', 'Senior')
+f1.bater_ponto()
+inspect(f1, methods=True)
