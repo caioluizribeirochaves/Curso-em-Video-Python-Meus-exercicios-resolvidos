1303d56d0f2aa0e26ff43427738a1b5f6cd354d0
Caio Luiz <caioriberio@gmail.com>
2026-07-17 16:37:55 -0300

    Update __main__.py

--- PythonPOO/exercicios/ex015/__main__.py
+++ PythonPOO/exercicios/ex015/__main__.py
@@ -1,14 +1,23 @@
 from ex015 import *
 
 def main():
+
     c1 = Carteira(100)
     c2 = Carteira(100)
     c1 += 100
 
+    if (c1 <= c2):
+        print('A primeira carteira tem mais dinheiro')
+    else:
+        print('A segunda carteira tem mais dinheiro')
 
-    if (c1 == c2)
+    if (c1 == c2):
+        print('Vocês tem o mesmo valor na carteira')
+    else:
+        print('As carteiras tem valores diferentes')
+
     print(c1)
-
+    print(c2)
 
 if __name__ == '__main__':
     main()
